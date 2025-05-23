e5d29628acbbb41f0d5b3eb8a55effdc5622d27c
Keita Nonaka <iKonnyaku40@gmail.com>
2025-05-23 11:35:32 -0700

    test: test xsumlarge and xsumauto as well as xsumsmall

--- TestRunner.h
+++ TestRunner.h
@@ -7,9 +7,11 @@ namespace Test {
 
 namespace {
 
-void sameValue(std::vector<double> vec, double expected);
+void isValid(double actual, double expected);
 
-}
+void sameValue(std::vector<double> &vec, double expected);
+
+} // namespace
 
 void runTest();
 
